af55278781549f9ec6b81fa4b58f71ca30ab064b
Artur Carvalho <artur.carvalho@nxp.com>
2022-03-23 09:05:47 +0100

    fix typo

--- infrastructure/modules/spoke/function_app.tf
+++ infrastructure/modules/spoke/function_app.tf
@@ -18,7 +18,7 @@ resource "azurerm_app_service_plan" "delete_posts" {
 }
 
 resource "azurerm_function_app" "delete_posts" {
-  name                       = "delete_posts"
+  name                       = "deleteposts"
   location                   = azurerm_resource_group.this.location
   resource_group_name        = azurerm_resource_group.this.name
   app_service_plan_id        = azurerm_app_service_plan.delete_posts.id
