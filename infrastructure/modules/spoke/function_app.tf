3d23410a76b1fe16308d23f537eff0873030a785
Artur Carvalho <artur.carvalho@nxp.com>
2022-03-23 10:08:05 +0100

    update name

--- infrastructure/modules/spoke/function_app.tf
+++ infrastructure/modules/spoke/function_app.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "function_app" {
-  name                     = "functionsappsa"
+  name                     = "funcappghostwebapp"
   resource_group_name      = azurerm_resource_group.this.name
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
